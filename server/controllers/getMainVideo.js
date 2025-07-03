997360e184905853980b647018babf3d04e84992
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-03 15:19:33 -0700

    server: added fs to getVideoList and getMainVideo to ensure it's always reading the latest video data, including new video added through POST /videos and confirmed to be working on postman

--- server/controllers/getMainVideo.js
+++ server/controllers/getMainVideo.js
@@ -1,10 +1,21 @@
 // returns one main video
 // checked on postman and this works http://localhost:8080/videos/1a4kjruuedd9
+// To make sure you’re always reading the latest video data, including new videos added through POST /videos.
 
-const videos = require("../data/videos.json");
+const fs = require("fs");
+const path = require("path");
 
-function getMainVideo(id) {
-  return videos.find((video) => video.id === id);
+const filePath = path.join(__dirname, "../data/videos.json");
+
+function getMainVideo(req, res) {
+  const videos = JSON.parse(fs.readFileSync(filePath));
+  const video = videos.find((video) => video.id === req.params.id);
+
+  if (!video) {
+    return res.status(404).json({ message: "Video not found" });
+  }
+
+  res.json(video);
 }
 
 module.exports = getMainVideo;
--- server/controllers/getVideoList.js
+++ server/controllers/getVideoList.js
@@ -1,15 +1,32 @@
 // returns all the videos
 // checked on postman and this works http://localhost:8080/videos
+// To make sure you’re always reading the latest video data, including new videos added through POST /videos.
 
-const videos = require("../data/videos.json");
 
-function getVideoList() {
-    return videos.map((video) => ({
-        id: video.id,
-        title: video.title,
-        channel: video.channel,
-        image: video.image,
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "../data/videos.json");
+
+function getVideoList(req, res) {
+  console.log("👉 /videos route triggered");
+
+  try {
+    const videos = JSON.parse(fs.readFileSync(filePath));
+    console.log("✅ videos loaded");
+
+    const videoList = videos.map((video) => ({
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
     }));
+
+    res.json(videoList); // Don't forget this line
+  } catch (error) {
+    console.error("❌ Error reading video list:", error.message);
+    res.status(500).json({ message: "Failed to load videos." });
+  }
 }
 
 module.exports = getVideoList;
