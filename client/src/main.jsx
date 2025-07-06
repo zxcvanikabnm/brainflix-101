465e6d0bd556effefe6c216ab9ca22254d77c3bf
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-07-06 00:45:30 -0700

    client: set up structure in Main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/global.scss";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </StrictMode>
+);
